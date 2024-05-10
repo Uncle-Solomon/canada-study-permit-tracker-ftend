d85e6353349df887ccb3440c67ea1006dc7cb977
Ameh Solomon Onyeke <amehsolomon46@gmail.com>
2024-05-10 09:54:52 +0100

    done

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,14 +1,14 @@
 export interface CaseProps {
   username: string;
-  application_date: Date;
+  application_date?: Date | null;
   application_status?: string;
   biometric_status?: string;
-  biometric_date: Date;
+  biometric_date?: Date | null;
   medical_status?: string;
-  medical_date: Date;
+  medical_date?: Date | null;
   elegibility_status?: string;
-  elegibility_date: Date;
+  elegibility_date?: Date | null;
   background_check_status?: string;
-  background_check_date: Date;
+  background_check_date?: Date | null;
   ppr_request?: string;
 }
